d0cfe7f3c28d842a8208ebff2b30b7517b47f4bf
Sreenath285 <sreenath.285@gmail.com>
2022-03-25 13:45:25 +0530

    Getting invoice by giving user ID

---
+++ CabInvoiceGenerator/src/main/java/InvoiceSummary.java
@@ -0,0 +1,22 @@
+import java.util.Objects;
+
+public class InvoiceSummary {
+
+    private int noOfRides;
+    private double totalFare;
+    private double average;
+
+    public InvoiceSummary(int noOfRides, double totalFare) {
+        this.noOfRides = noOfRides;
+        this.totalFare = totalFare;
+        this.average = this.totalFare / this.noOfRides;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return noOfRides == that.noOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.average, average) == 0;
+    }
+}
---
+++ CabInvoiceGenerator/src/main/java/Ride.java
@@ -0,0 +1,15 @@
+public class Ride {
+
+    double distance;
+    int time;
+
+    /***
+     * created parameterized constructor
+     * @param distance - to pass distance
+     * @param time - to pass time
+     */
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
