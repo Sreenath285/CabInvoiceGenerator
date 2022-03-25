2de599fc98e724fbbaff4d99df32b73d6d63a91e
Sreenath285 <sreenath.285@gmail.com>
2022-03-25 11:04:54 +0530

    Calculating total fare for the journey

--- CabInvoiceGenerator/src/main/java/InvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/InvoiceGenerator.java
@@ -1,6 +1,7 @@
 public class InvoiceGenerator {
 
-    private static final double MINIMUM_COST_PER_KM = 10;
+    private static final double MINIMUM_FARE = 5;
+    private static double MINIMUM_COST_PER_KM = 10;
     private static int COST_PER_TIME = 1;
 
     /***
@@ -11,6 +12,6 @@ public class InvoiceGenerator {
      */
     public double calculateFare(double distance, int time) {
         double totalFare =  distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        return totalFare;
+        return Math.max(MINIMUM_FARE, totalFare);
     }
 }
